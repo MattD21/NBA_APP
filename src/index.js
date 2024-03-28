6aea1f79e1e07a8db3115de5952f7d57c37d84cc
Matthew Darragh <mattdarragh@icloud.com>
2024-03-27 21:14:02 -0500

    fix: ranking on player table and  refactoring

--- src/index.js
+++ src/index.js
@@ -16,23 +16,16 @@ con.connect(function(err) {
 });
 
 const app = express();
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', async function(req, res) {
     // current timestamp in milliseconds
     const timestamp = Date.now();
-    const dateObject = new Date(timestamp);
-    let date = dateObject.getDate();
-    const month = dateObject.getMonth() + 1;
-    const year = dateObject.getFullYear();
-    if(date < 10) {
-        date = '0' + date;
-    }
-    // prints date & time in YYYY-MM-DD format
-    const full_date = `${year}-${month}-${date}`;
-    console.log(full_date);
+    const full_date = date(timestamp);
+    
     let param_Date = "dates[]=" + full_date;
-    const url = 'https://www.balldontlie.io/api/v1/games?' + param_Date;
+    const url = 'https://api.balldontlie.io/v1/games?' + param_Date;
     const options = {
         method: 'GET',
         url: url,
@@ -48,8 +41,30 @@ app.get('/', async function(req, res) {
 	    console.error(error);
     }
 });
-app.get('/games', function(req, res) {
-    res.render('pages/games');
+app.get('/games', async function(req, res) {
+    let timestamp = Date.now();
+    const end_date = date(timestamp);
+
+    timestamp = Date.now() - 7 * 24 * 60 * 60 * 1000;
+    const start_date = date(timestamp);
+    
+    const params = "start_date=" + start_date;
+    const url = 'https://api.balldontlie.io/v1/games?per_page=100&' + params;
+    const options = {
+        method: 'GET',
+        url: url,
+        headers: {
+          Authorization: apiKeys.balldontlie
+        }
+      };
+
+    try {
+	    const response = await axios.request(options);
+        console.log(response.data);
+        res.render('pages/games.ejs', {games: response.data, teams});
+    } catch (error) {
+	    console.error(error);
+    }
 });
 app.get('/players', async function(req, res) {
     con.query("SELECT players.player_name, players.points, players.assists, players.rebounds, teams.team_name FROM players INNER JOIN teams on teams.id=players.team_id", function (err, result, fields) {
@@ -58,7 +73,7 @@ app.get('/players', async function(req, res) {
 });
 app.get('/teams', async function(req, res) {
 
-    const options = {
+    const options_rapid = {
         method: 'GET',
         url: 'https://api-nba-v1.p.rapidapi.com/standings',
         params: {
@@ -72,7 +87,7 @@ app.get('/teams', async function(req, res) {
       };
       
       try {
-          const response = await axios.request(options);
+          const response = await axios.request(options_rapid);
           const tmp = response.data;
           res.render('pages/teams', {teams: response.data});
       } catch (error) {
@@ -82,3 +97,20 @@ app.get('/teams', async function(req, res) {
 app.listen(8080, () => {
     console.log('server is running!')
 })
+
+function date(timestamp) {
+    const dateObject = new Date(timestamp);
+    let date = dateObject.getDate();
+    let month = dateObject.getMonth() + 1;
+    const year = dateObject.getFullYear();
+    if(date < 10) {
+        date = '0' + date;
+    }
+    if(month < 10) {
+        month = '0' + month;
+    }
+    // prints date & time in YYYY-MM-DD format
+    const result = `${year}-${month}-${date}`;
+    console.log(result);
+    return result;
+}
